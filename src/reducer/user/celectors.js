ada810aca972a444d3c007cef34d8724d0e47753
shishlachevilya <shishlachevilya@gmail.com>
2020-08-02 00:09:31 +0300

    7.1. tests

--- src/reducer/user/celectors.js
+++ src/reducer/user/celectors.js
@@ -1,6 +1,6 @@
 import NameSpace from "../name-space";
 
-const NAME_SPACE = NameSpace.USER;
+// const NAME_SPACE = NameSpace.USER;
 
 export const getAuthorizationStatus = (state) => {
   return state[NameSpace].authorizationStatus;
--- src/testData.js
+++ src/testData.js
@@ -1,6 +1,7 @@
 export const movies = [
   {
     id: `1`,
+    bg: `bg`,
     title: `Mindhunter`,
     videoPrev: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
     preview: `img/mindhunter.jpg`,
@@ -11,7 +12,7 @@ export const movies = [
     rating: 10.0,
     votes: 32,
     director: `Best Director`,
-    starring: `Nicola Cage, Cara Delvin, Joseph Moser`,
+    starring: [`Nicola Cage`, `Cara Delvin`, `Joseph Moser`],
     duration: 48,
     release: 2016,
     reviews: [
@@ -51,120 +52,6 @@ export const movies = [
         rating: 5.2,
         date: `2019-05-08`,
       }
-    ]
-  },
-  {
-    id: `2`,
-    title: `Orlando`,
-    videoPrev: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
-    preview: `img/orlando.jpg`,
-    poster: `img/orlando.jpg`,
-    background: `img/orlando.jpg`,
-    genre: `Comedies`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci. Donec cursus lectus auctor quam tempus ultricies. Nullam at aliquam massa. Donec mollis quis urna eget condimentum. Mauris at dictum ante. Phasellus maximus massa augue, ut luctus dolor rutrum et. Aliquam faucibus turpis lectus, sed hendrerit dolor malesuada at.`,
-    rating: 9.7,
-    votes: 124,
-    director: `Best Director`,
-    starring: `Nicola Cage, Cara Delvin, Joseph Moser`,
-    duration: 156,
-    release: 2017,
-    reviews: [
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `Camille Guaty`,
-        rating: 8.4,
-        date: `2019-05-08`,
-      },
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `John Dou`,
-        rating: 3.4,
-        date: `2019-05-08`,
-      },
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `Chris Hemsworth`,
-        rating: 5.2,
-        date: `2019-05-08`,
-      },
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `Chris Hemsworth`,
-        rating: 5.2,
-        date: `2019-05-08`,
-      }
-    ]
-  },
-  {
-    id: `3`,
-    title: `Dardjeeling Limited`,
-    preview: `img/dardjeeling-limited.jpg`,
-    poster: `img/dardjeeling-limited.jpg`,
-    background: `img/dardjeeling-limited.jpg`,
-    genre: `Thrillers`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci. Donec cursus lectus auctor quam tempus ultricies. Nullam at aliquam massa. Donec mollis quis urna eget condimentum. Mauris at dictum ante. Phasellus maximus massa augue, ut luctus dolor rutrum et. Aliquam faucibus turpis lectus, sed hendrerit dolor malesuada at.`,
-    rating: 8.0,
-    votes: 170,
-    director: `Best Director`,
-    starring: `Nicola Cage, Cara Delvin, Joseph Moser`,
-    duration: 98,
-    videoPrev: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
-    release: 1999,
-    reviews: [
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `Camille Guaty`,
-        rating: 8.4,
-        date: `2019-05-08`,
-      },
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `John Dou`,
-        rating: 3.4,
-        date: `2019-05-08`,
-      },
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `Chris Hemsworth`,
-        rating: 5.2,
-        date: `2019-05-08`,
-      },
-    ]
-  },
-  {
-    id: `4`,
-    title: `Midnight Special`,
-    preview: `img/midnight-special.jpg`,
-    poster: `img/midnight-special.jpg`,
-    background: `img/midnight-special.jpg`,
-    genre: `Sci-Fi`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci. Donec cursus lectus auctor quam tempus ultricies. Nullam at aliquam massa. Donec mollis quis urna eget condimentum. Mauris at dictum ante. Phasellus maximus massa augue, ut luctus dolor rutrum et. Aliquam faucibus turpis lectus, sed hendrerit dolor malesuada at.`,
-    rating: 7.8,
-    votes: 52,
-    director: `Best Director`,
-    starring: `Nicola Cage, Cara Delvin, Joseph Moser`,
-    duration: 102,
-    videoPrev: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
-    release: 2001,
-    reviews: [
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `Camille Guaty`,
-        rating: 8.4,
-        date: `2019-05-08`,
-      },
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `John Dou`,
-        rating: 3.4,
-        date: `2019-05-08`,
-      },
-      {
-        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius quis lorem id iaculis. Sed eget luctus orci.`,
-        author: `Chris Hemsworth`,
-        rating: 5.2,
-        date: `2019-05-08`,
-      },
     ]
   }
 ];
